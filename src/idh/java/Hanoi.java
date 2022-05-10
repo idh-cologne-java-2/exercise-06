13b752d1d1a238898355608f76044e9c99a3c1b2
Nils Reiter <nils.reiter@uni-koeln.de>
2022-05-10 20:56:06 +0200

    removed unneeded imports

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,9 +3,7 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.Iterator;
-import java.util.LinkedList;
 
 public class Hanoi {
 

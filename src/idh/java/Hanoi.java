4e69bb907153a884180854f066395f6623eab737
sophiejorns <sophiejorns@macbook-air-sophie.fritz.box>
2022-05-12 13:43:39 +0200

    towers of Hanoi is playable

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -3,17 +3,41 @@ package idh.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Hanoi {
+	
+	
+	Stack<Integer> towerL = new Stack<Integer>();
+	Stack<Integer> towerM = new Stack<Integer>();
+	Stack<Integer> towerR = new Stack<Integer>();
 
+	
 	public Hanoi() {
-		// TODO: Implement
+		ArrayList<Integer> p = new ArrayList<Integer>(9);
+		p.add(9);
+		p.add(8);
+		p.add(7);
+		p.add(6);
+		p.add(5);
+		p.add(4);
+		p.add(3);
+		p.add(2);
+		p.add(1);
 	}
 	
-	private void movePiece(char from, char to) {
-		// TODO: Implement
-	}
+		
+	private void movePiece(int n, char from, char to) {
+		if (n == 1) {
+			System.out.println("Move piece 1 from" + from + "to" + to);
+			}
+		movePiece(n-1, from, to);
+		System.out.println("Move piece" + n + "from" + from + "to" + to);
+		
+	
+	}	
 	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,20 +58,83 @@ public class Hanoi {
 		}
 	}
 	
+	
+	private void movePiece(char source, char target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			Stack<Integer> tL = towerL;
+			int n = 0;
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return tL.size() > n;
+			}
+
+			@Override
+			public Integer next() {
+				// TODO Auto-generated method stub
+				int i = tL.get(n);;
+				n++;
+				return i;
+			}
+			
+		};
 
 	}
+
+	
 	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			Stack<Integer> tM = towerM;
+			int n = 0;
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return tM.size() > n;
+			}
+
+			@Override
+			public Integer next() {
+				// TODO Auto-generated method stub
+				int i = tM.get(n);
+				n++;
+				return i;
+			}
+			
+		};
 
 	}
+	
 	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			Stack<Integer> tR = towerR;
+			int n = 0;
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return tR.size() > n;
+			}
+
+			@Override
+			public Integer next() {
+				// TODO Auto-generated method stub
+				int i = tR.get(n);;
+				n++;
+				return i;
+			}
+			
+		};
 	}
+	
+	
 	
 	public String toString() {
 		StringBuilder b = new StringBuilder();
@@ -74,9 +161,12 @@ public class Hanoi {
 		return b.toString();
 	}
 	
+	
+	
 	public static void main(String[] args) {
 		Hanoi hanoi = new Hanoi();
 		hanoi.run();
 	}
 
 }
+

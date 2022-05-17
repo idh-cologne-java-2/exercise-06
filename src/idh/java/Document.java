b21605f4495854063b761f59603a992b162da8bf
Jorine Twint <jorine.twint@uni-bayreuth.de>
2022-05-17 20:25:07 +0200

    Aufgabe 6 erledigt, hoffe es passt

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+
 import java.util.Iterator;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -12,6 +13,8 @@ public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
+		
+		
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
@@ -22,30 +25,56 @@ public class Document implements Iterable<String> {
 		Document doc = new Document();
 		doc.documentText = b.toString();
 		
+		
+		
 		return doc;
 	}
 	
 	public String getDocumentText() {
 		return documentText;
+		
 	}
 
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
 	
-	public static final void main(String[] args) throws IOException {
+	
+	public static final void main(String[] args) throws IOException {   // Hier wird die ttr Methode für das Dracula Dokument aufgerufen
 		Document d = Document.readFromFile(new File("data/dracula.txt"));
 		int i = 0;
 		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
+//			System.out.println(i++ + ": " + token + " ");
+			i++;
+//			if (i > 100)
+//				break;
 		}
+		System.out.println(i);
+		System.out.println(d.ttr(d));
 	}
+	
+	
+	
+	public double ttr(Document document) {  		// dies ist die Methode die dem Dokument übergeben werden muss
+		Set<String> types = new HashSet<String>();
+		double ratio = 0;
+		double tokens = 0;
+		for (String s : document) {
+			types.add(s);
+			tokens++;  				////bei jedem Durchgang wird die Anzahl der Token erhöht
+		}
+		ratio = types.size() / tokens;
+		return ratio;
+	}
+	
+	
 
 	@Override
 	public Iterator<String> iterator() {
 		return new Iterator<String>() {
+			
+			
+			
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
 			
@@ -61,6 +90,8 @@ public class Document implements Iterable<String> {
 			
 		};
 	}
+	
+
 	
 	
 }
--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -1,18 +1,64 @@
 package idh.java;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Hanoi {
-
+	
+	
+	Stack <Integer> stackl = new Stack<Integer>();
+	Stack <Integer> stackm = new Stack<Integer>();
+	Stack <Integer> stackr = new Stack<Integer>();
+	ArrayList<Stack<Integer>> stacks = new ArrayList<Stack<Integer>>();
+	
+	
+	
 	public Hanoi() {
-		// TODO: Implement
+		for (int i = 9; i > 0; i--) {
+			stackl.add(i);
+		}
+		
+		
+		stacks.add(stackl);
+		stacks.add(stackm);
+		stacks.add(stackr);
+			
 	}
 	
 	private void movePiece(char from, char to) {
-		// TODO: Implement
+		int i = 0;
+		int j = 0;
+		switch (from) {
+		case 'l':
+		   i = 0;
+		  break;
+		case 'm':
+		   i = 1;
+		  break;
+		case 'r':
+		  i = 2;
+		  break;
+		}
+		switch (to) {
+		case 'l':
+		  j = 0;
+		  break;
+		case 'm':
+		  j = 1;
+		  break;
+		case 'r':
+		  j = 2;
+	      break;
+		}	
+	if (stacks.get(j).isEmpty() || (stacks.get(j).peek() > stacks.get(i).peek())) {
+		stacks.get(j).push(stacks.get(i).pop()); 
+	}
+	else {
+	System.out.println("Against the rules!");	
+	}
 	}
 	
 	public void run() {
@@ -27,6 +73,10 @@ public class Hanoi {
 					char target = s.charAt(1);
 					movePiece(source, target);
 				}
+				if (stackl.isEmpty() && stackm.isEmpty()) {
+					System.out.println("You have won!");
+					break;
+				}
 			} catch (Exception e) {
 				System.out.println("Try again, something's not right.");
 				e.printStackTrace();
@@ -34,42 +84,84 @@ public class Hanoi {
 		}
 	}
 	
-	private Iterator<Integer> getLeftDescendingIterator() {
-		// TODO: Implement
-		return null;
+	private Iterator<Integer> getLeftIterator() {
+		return new Iterator<Integer>() {
+			int i = 0;
+			
+			
+			@Override
+			public boolean hasNext() {
+				return  i < stacks.get(0).size();
+			}
+
+			@Override
+			public Integer next() {
+			return stacks.get(0).get(i++);
+			}
+			
+		};
 
 	}
-	private Iterator<Integer> getMiddleDescendingIterator() {
-		// TODO: Implement
-		return null;
+	private Iterator<Integer> getMiddleIterator() {
+		return new Iterator<Integer>() {
+			int i = 0;
+			
+			
+			@Override
+			public boolean hasNext() {
+				return  i < stacks.get(1).size();
+			}
+
+			@Override
+			public Integer next() {
+			return stacks.get(1).get(i++);
+			}
+			
+		};
 
 	}
-	private Iterator<Integer> getRightDescendingIterator() {
-		// TODO: Implement
-		return null;
+	private Iterator<Integer> getRightIterator() {
+		return new Iterator<Integer>() {
+			int i = 0;
+			
+			
+			@Override
+			public boolean hasNext() {
+				return  i < stacks.get(2).size();
+			}
+
+			@Override
+			public Integer next() {
+			return stacks.get(2).get(i++);
+			}
+			
+		};
 	}
 	
 	public String toString() {
 		StringBuilder b = new StringBuilder();
 		b.append("  |\n l|");
 		Iterator<Integer> iter;
-		iter = this.getLeftDescendingIterator();
+		iter = this.getLeftIterator();
 		while(iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
+			
 		}
 		b.append("\n  |\n m|");
-		iter = this.getMiddleDescendingIterator();
+		iter = this.getMiddleIterator();
 		while(iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
+		
 		b.append("\n  |\n r|");
-		iter = this.getRightDescendingIterator();
+		iter = this.getRightIterator();
 		while(iter.hasNext()) {
 			b.append(iter.next());
 			b.append(' ');
 		}
+		
 		b.append("\n  |");
 		return b.toString();
 	}

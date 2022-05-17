e18bc1d416021460362864d376decc18c593d953
Steffi S.-E. <steffix317@gmail.com>
2022-05-17 14:19:54 +0200

    TTR Berechnung hinzugefügt

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -41,6 +41,7 @@ public class Document implements Iterable<String> {
 			if (i > 100)
 				break;
 		}
+		System.out.println("TTR: " + d.ttr());
 	}
 
 	@Override
@@ -60,6 +61,31 @@ public class Document implements Iterable<String> {
 			}
 			
 		};
+	}
+	
+	private double ttr() throws IOException {
+		//String wird in Set gespeichert 
+		Set<String> set = new HashSet<String>(); 
+		//benötigen das Dokument
+		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		//eine Zählervariable
+		double counter = 0;
+		//Größe zunächst 0
+		double si = 0; 
+		//für jedes token in der Datei d...(token kann überall verwendet werden)
+		for (String token : d) {
+			//füge dem Set das token hinzu, wenn es nicht schon in dem Set existiert 
+			set.add(token);
+			//zähle weiter
+			counter++;
+		}
+		//Größe von dem Set
+		si = set.size();
+		
+		//zuletzt müssen alle Elemte die hinzugefügt wurden durch die Anzahl vom Counter geteilt werden.
+		//Hinzugefügt wurden nur Elemente die es noch nicht gibt, Counter hat jedoch alle Elemte gezählt, auch wenn es das schon gibt.
+		return si / counter;
+		
 	}
 	
 	

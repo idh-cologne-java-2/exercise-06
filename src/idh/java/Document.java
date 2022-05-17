7adcbf5a0d809e6b2ed245596488899a1d0a804a
JohannonBlake <johannon.blade@Gmail.com>
2022-05-17 13:36:41 +0200

    need a break
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaargh

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,10 +3,7 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Document implements Iterable<String> {
 	String documentText;
@@ -61,6 +58,285 @@ public class Document implements Iterable<String> {
 			
 		};
 	}
-	
-	
+
+
+
+
+	public class MyLinkedList<T> implements LinkedList<T> {
+
+		/**
+		 * We only need to store the very first element of our list,
+		 * because it will now whether there is a next element.
+		 */
+		ListElement first;
+
+
+		@Override
+		public int size() {
+			// TODO Implement!
+			return 0;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return first == null;
+		}
+
+		@Override
+		public boolean contains(Object o) {
+			// TODO Implement!
+			return false;
+		}
+
+		@Override
+		public Iterator<T> iterator() {
+			return new Iterator<T>() {
+				ListElement next = first;
+
+				@Override
+				public boolean hasNext() {
+					return next != null;
+				}
+
+				@Override
+				public T next() {
+					T ret = next.value;
+					next = next.next;
+					return ret;
+				}
+
+			};
+		}
+
+		@Override
+		public Object[] toArray() {
+			// TODO Implement!
+			return null;
+		}
+
+		@Override
+		public <E> E[] toArray(E[] a) {
+			if (a.length < size()) {
+				a = (E[]) new Object[size()];
+			}
+			int i = 0;
+			for (T t : this) {
+				a[i++] = (E) t;
+			}
+			return a;
+		}
+
+		@Override
+		public boolean add(T e) {
+			ListElement newListElement = new ListElement(e);
+			if (first == null)
+				first = newListElement;
+			else
+				last().next = newListElement;
+			return true;
+		}
+
+		@Override
+		public boolean remove(Object o) {
+			// TODO: Implement
+			return false;
+		}
+
+		@Override
+		public boolean containsAll(Collection<?> c) {
+			for (Object o : c)
+				if (! contains(o))
+					return false;
+			return true;
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends T> c) {
+			for (T t : c)
+				this.add(t);
+			return true;
+		}
+
+		@Override
+		public boolean addAll(int index, Collection<? extends T> c) {
+			// TODO Implement!
+			return false;
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			boolean r = true;
+			for (Object o : c)
+				r = r || this.remove(o);
+			return r;
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void clear() {
+			first = null;
+		}
+
+		@Override
+		public T get(int index) {
+			return getElement(index).value;
+		}
+
+		@Override
+		public T set(int index, T element) {
+			// TODO: Implement
+			return null;
+		}
+
+		@Override
+		public void add(int index, T element) {
+			// TODO: Implement
+		}
+
+		@Override
+		public T remove(int index) {
+			// TODO: Implement
+			return null;
+		}
+
+		@Override
+		public int indexOf(Object o) {
+			// TODO: Implement
+			return 0;
+		}
+
+		@Override
+		public int lastIndexOf(Object o) {
+			// TODO: Implement
+			return 0;
+		}
+
+		@Override
+		public ListIterator<T> listIterator() {
+			return new ListIterator<T>() {
+
+				ListElement previous = null;
+				ListElement next = first;
+				int index;
+
+				@Override
+				public boolean hasNext() {
+					return next != null;
+				}
+
+				@Override
+				public T next() {
+					previous = next;
+					T ret = next.value;
+					next = next.next;
+					index++;
+					return ret;
+				}
+
+				@Override
+				public boolean hasPrevious() {
+					return false;
+				}
+
+				@Override
+				public T previous() {
+					throw new UnsupportedOperationException();
+				}
+
+				@Override
+				public int nextIndex() {
+					return index+1;
+				}
+
+				@Override
+				public int previousIndex() {
+					return index-1;
+				}
+
+				@Override
+				public void remove() {
+					previous.next = next.next;
+				}
+
+				@Override
+				public void set(T e) {
+					next.value = e;
+				}
+
+				@Override
+				public void add(T e) {
+					throw new UnsupportedOperationException();
+				}
+
+			};
+		}
+
+		@Override
+		public ListIterator<T> listIterator(int index) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public List<T> subList(int fromIndex, int toIndex) {
+			throw new UnsupportedOperationException();
+		}
+
+		private class ListElement {
+			T value;
+			ListElement next;
+
+			ListElement(T value) {
+				this.value = value;
+			}
+		}
+
+		/**
+		 * Internal method that iterates over the list, returning the last element (i.e., the one whose next field is null)
+		 * @return
+		 */
+		private ListElement last() {
+			if (first == null)
+				return null;
+			ListElement current = first;
+
+			while(current.next != null) {
+				current = current.next;
+			}
+			return current;
+		}
+
+		/**
+		 * Internal method to get the list element (not the value) of the list at the specified index position.
+		 * @param index
+		 * @return
+		 */
+		private ListElement getElement(int index) {
+			if (isEmpty())
+				return null;
+			ListElement current = first;
+			while(current != null) {
+				if (index == 0)
+					return current;
+				index--;
+				current = current.next;
+			}
+			return null;
+		}
+
+		public static void main(String[] args) {
+			MyLinkedList<String> ll = new MyLinkedList<String>();
+			ll.add("Hallo");
+			ll.add("Welt");
+			ll.get(0);
+			for (String s : ll) {
+				System.out.println(s);
+			}
+		}
+	}
+
+
 }
--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -1,18 +1,43 @@
 package idh.java;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Hanoi {
 
 	public Hanoi() {
 		// TODO: Implement
+		Queue<Integer> left = new LinkedList<Integer>();
+		left.add(1);
+		left.add(2);
+		/*
+		left.add("3");
+		left.add("4");
+		left.add("5");
+		left.add("6");
+		left.add("7");
+		left.add("8");
+		left.add("9");
+		*/
+
+//the first stack on the left to begin the game with
+		Queue<String> middle = new LinkedList<String>();
+		Queue<String> right = new LinkedList<String>();
+//the other two stacks for the game
+
+
 	}
+
+
 	
 	private void movePiece(char from, char to) {
+
+	middle.add();
 		// TODO: Implement
+
 	}
 	
 	public void run() {
@@ -36,16 +61,21 @@ public class Hanoi {
 	
 	private Iterator<Integer> getLeftDescendingIterator() {
 		// TODO: Implement
+		Iterator<Integer> itLeft = new left.iterator();
+
 		return null;
 
 	}
 	private Iterator<Integer> getMiddleDescendingIterator() {
 		// TODO: Implement
+
+		Iterator<Integer> itMiddle = new middle.iterator();
 		return null;
 
 	}
 	private Iterator<Integer> getRightDescendingIterator() {
 		// TODO: Implement
+		iterator<Integer> itRight = new right.iterator();
 		return null;
 	}
 	

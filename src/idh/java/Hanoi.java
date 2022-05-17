09d189c33044aabfa4d6ab0679144bdf19d0b48c
Steffi S.-E. <steffix317@gmail.com>
2022-05-17 18:10:43 +0200

    Hanoi Spiel gelöst

--- src/idh/java/Hanoi.java
+++ src/idh/java/Hanoi.java
@@ -4,15 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class Hanoi {
+	
+	Stack<Integer> l = new Stack<Integer>();
+	Stack<Integer> m = new Stack<Integer>();
+	Stack<Integer> r = new Stack<Integer>();
 
 	public Hanoi() {
 		// TODO: Implement
+		for(int i = 9;i > 0; i--) {
+			l.push(i);
+		}
+		
 	}
 	
 	private void movePiece(char from, char to) {
 		// TODO: Implement
+		if (from == 'l' && to == 'm') {
+			m.push(l.pop());
+		} else if (from == 'l' && to == 'r'){
+			r.push(l.pop());
+		} else if (from == 'm' && to == 'r'){
+			r.push(m.pop());
+		} else if (from == 'm' && to == 'l'){
+			l.push(m.pop());
+		} else if (from == 'r' && to == 'm'){
+			m.push(r.pop());
+		} else if (from == 'r' && to == 'l'){
+			l.push(r.pop());
+		}
+		
 	}
 	
 	public void run() {
@@ -36,17 +59,69 @@ public class Hanoi {
 	
 	private Iterator<Integer> getLeftDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			Stack<Integer> sl = l;
+			int counter = 0;
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return counter < sl.size();
+			}
+
+			@Override
+			public Integer next() {
+				// TODO Auto-generated method stub
+				int c = sl.get(counter);
+				counter++;
+				return c;
+			}
+		};
 
 	}
 	private Iterator<Integer> getMiddleDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			Stack<Integer> sm = m;
+			int counter = 0;
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return counter < sm.size();
+			}
+
+			@Override
+			public Integer next() {
+				// TODO Auto-generated method stub
+				int c = sm.get(counter);
+				counter++;
+				return c;
+			}
+			
+		};
 
 	}
 	private Iterator<Integer> getRightDescendingIterator() {
 		// TODO: Implement
-		return null;
+		return new Iterator<Integer>() {
+			Stack<Integer> sr = r;
+			int counter = 0;
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return counter < sr.size();
+			}
+
+			@Override
+			public Integer next() {
+				// TODO Auto-generated method stub
+				int c = sr.get(counter);
+				counter++;
+				return c;
+			}
+		};
 	}
 	
 	public String toString() {
